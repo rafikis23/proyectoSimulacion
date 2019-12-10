b7fcda70db08dd00fee5cf6dc1aef79e109fa101
rafikis23 <leocasco23@gmail.com>
2019-12-10 10:50:14 -0600

    Create proyecto.py

---
+++ proyecto.py
@@ -0,0 +1,369 @@
+def funcionAtlantida1():
+	valorCalcular = int(input('Ingrese el valor que desea ahorrar:'))
+	if valorCalcular>= 5000 and valorCalcular <= 10000:
+		interes = 0.0025
+		formulaCompuesto = (((1+(interes/4))**4)-1)
+		bPrimer=(1+formulaCompuesto)**1
+		bSegundo=(1+formulaCompuesto)**2
+		bTercer=(1+formulaCompuesto)**3
+		cPrimer=valorCalcular*bPrimer
+		cSegundo=valorCalcular*bSegundo
+		cTercer=valorCalcular*bTercer
+ 
+		formulaDoce= cPrimer/12
+		formulaVeinti=cSegundo/24
+		formula36=cTercer/36
+	
+		print ('Las cuotas que debe realizar en Atlantida a 12 cuotas son:', formulaDoce)
+		print('Las cuotas que debe realizar en Atlantida a 24 cuotas son:', formulaVeinti)
+		print('Las cuotas que debe realizar en Atlantida a 36 cuotas son:', formula36)
+		
+		
+	else:
+		print('El valor no es posible calcular')
+
+def funcionAtlantida2():
+	valorCalcular = int(input('Ingrese el valor que desea ahorrar:'))
+	if valorCalcular>= 10000.01 and valorCalcular <= 50000:
+		interes = 0.0375
+		formulaCompuesto = (((1+(interes/4))**4)-1)
+		bPrimer=(1+formulaCompuesto)**1
+		bSegundo=(1+formulaCompuesto)**2
+		bTercer=(1+formulaCompuesto)**3
+		cPrimer=valorCalcular*bPrimer
+		cSegundo=valorCalcular*bSegundo
+		cTercer=valorCalcular*bTercer
+ 
+		formulaDoce= cPrimer/12
+		formulaVeinti=cSegundo/24
+		formula36=cTercer/36
+	
+		print ('Las cuotas que debe realizar en Atlantida a 12 cuotas son:', formulaDoce)
+		print('Las cuotas que debe realizar en Atlantida a 24 cuotas son:', formulaVeinti)
+		print('Las cuotas que debe realizar en Atlantida a 36 cuotas son:', formula36)
+		
+		
+	else:
+		print('El valor no es posible calcular')
+		
+
+def funcionAtlantida3():
+	valorCalcular = int(input('Ingrese el valor que desea ahorrar:'))
+	if valorCalcular>= 50000.01 and valorCalcular <= 100000:
+		interes = 0.0450
+		formulaCompuesto = (((1+(interes/4))**4)-1)
+		bPrimer=(1+formulaCompuesto)**1
+		bSegundo=(1+formulaCompuesto)**2
+		bTercer=(1+formulaCompuesto)**3
+		cPrimer=valorCalcular*bPrimer
+		cSegundo=valorCalcular*bSegundo
+		cTercer=valorCalcular*bTercer
+ 
+		formulaDoce= cPrimer/12
+		formulaVeinti=cSegundo/24
+		formula36=cTercer/36
+	
+		print ('Las cuotas que debe realizar en Atlantida a 12 cuotas son:', formulaDoce)
+		print('Las cuotas que debe realizar en Atlantida a 24 cuotas son:', formulaVeinti)
+		print('Las cuotas que debe realizar en Atlantida a 36 cuotas son:', formula36)
+		
+		
+	else:
+		print('El valor no es posible calcular')
+		
+def funcionAtlantida4():
+	valorCalcular = int(input('Ingrese el valor que desea ahorrar:'))
+	if valorCalcular>= 100000.01 and valorCalcular <= 500000:
+		interes = 0.0450
+		formulaCompuesto = (((1+(interes/4))**4)-1)
+		bPrimer=(1+formulaCompuesto)**1
+		bSegundo=(1+formulaCompuesto)**2
+		bTercer=(1+formulaCompuesto)**3
+		cPrimer=valorCalcular*bPrimer
+		cSegundo=valorCalcular*bSegundo
+		cTercer=valorCalcular*bTercer
+ 
+		formulaDoce= cPrimer/12
+		formulaVeinti=cSegundo/24
+		formula36=cTercer/36
+	
+		print ('Las cuotas que debe realizar en Atlantida a 12 cuotas son:', formulaDoce)
+		print('Las cuotas que debe realizar en Atlantida a 24 cuotas son:', formulaVeinti)
+		print('Las cuotas que debe realizar en Atlantida a 36 cuotas son:', formula36)
+		
+		
+	else:
+		print('El valor no es posible calcular')
+		
+def funcionAtlantida5():
+	valorCalcular = int(input('Ingrese el valor que desea ahorrar:'))
+	if valorCalcular>= 500000.01 and valorCalcular <= 1000000:
+		interes = 0.0450
+		formulaCompuesto = (((1+(interes/4))**4)-1)
+		bPrimer=(1+formulaCompuesto)**1
+		bSegundo=(1+formulaCompuesto)**2
+		bTercer=(1+formulaCompuesto)**3
+		cPrimer=valorCalcular*bPrimer
+		cSegundo=valorCalcular*bSegundo
+		cTercer=valorCalcular*bTercer
+ 
+		formulaDoce= cPrimer/12
+		formulaVeinti=cSegundo/24
+		formula36=cTercer/36
+	
+		print ('Las cuotas que debe realizar en Atlantida a 12 cuotas son:', formulaDoce)
+		print('Las cuotas que debe realizar en Atlantida a 24 cuotas son:', formulaVeinti)
+		print('Las cuotas que debe realizar en Atlantida a 36 cuotas son:', formula36)
+		
+		
+	else:
+		print('El valor no es posible calcular')
+def funcionOccidente1():
+	valorCalcular = int(input('Ingrese el valor que desea ahorrar:'))
+	if valorCalcular>= 500.01 and valorCalcular <= 50000:
+		interes = 0.035
+		formulaCompuesto = (((1+(interes/4))**4)-1)
+		bPrimer=(1+formulaCompuesto)**1
+		bSegundo=(1+formulaCompuesto)**2
+		bTercer=(1+formulaCompuesto)**3
+		cPrimer=valorCalcular*bPrimer
+		cSegundo=valorCalcular*bSegundo
+		cTercer=valorCalcular*bTercer
+ 
+		formulaDoce= cPrimer/12
+		formulaVeinti=cSegundo/24
+		formula36=cTercer/36
+	
+		print ('Las cuotas que debe realizar en Occidente a 12 cuotas son:', formulaDoce)
+		print('Las cuotas que debe realizar en Occidente a 24 cuotas son:', formulaVeinti)
+		print('Las cuotas que debe realizar en Occidente a 36 cuotas son:', formula36)
+		
+		
+	else:
+		print('El valor no es posible calcular')
+def funcionOccidente2():
+	valorCalcular = int(input('Ingrese el valor que desea ahorrar:'))
+	if valorCalcular>= 50000.01 and valorCalcular <= 100000:
+		interes = 0.0450
+		formulaCompuesto = (((1+(interes/4))**4)-1)
+		bPrimer=(1+formulaCompuesto)**1
+		bSegundo=(1+formulaCompuesto)**2
+		bTercer=(1+formulaCompuesto)**3
+		cPrimer=valorCalcular*bPrimer
+		cSegundo=valorCalcular*bSegundo
+		cTercer=valorCalcular*bTercer
+ 
+		formulaDoce= cPrimer/12
+		formulaVeinti=cSegundo/24
+		formula36=cTercer/36
+	
+		print ('Las cuotas que debe realizar en Occidente a 12 cuotas son:', formulaDoce)
+		print('Las cuotas que debe realizar en Occidente a 24 cuotas son:', formulaVeinti)
+		print('Las cuotas que debe realizar en Occidente a 36 cuotas son:', formula36)
+		
+		
+	else:
+		print('El valor no es posible calcular')
+def funcionOccidente3():
+	valorCalcular = int(input('Ingrese el valor que desea ahorrar:'))
+	if valorCalcular>= 100000.01 and valorCalcular <= 200000:
+		interes = 0.0500
+		formulaCompuesto = (((1+(interes/4))**4)-1)
+		bPrimer=(1+formulaCompuesto)**1
+		bSegundo=(1+formulaCompuesto)**2
+		bTercer=(1+formulaCompuesto)**3
+		cPrimer=valorCalcular*bPrimer
+		cSegundo=valorCalcular*bSegundo
+		cTercer=valorCalcular*bTercer
+ 
+		formulaDoce= cPrimer/12
+		formulaVeinti=cSegundo/24
+		formula36=cTercer/36
+	
+		print ('Las cuotas que debe realizar en Occidente a 12 cuotas son:', formulaDoce)
+		print('Las cuotas que debe realizar en Occidente a 24 cuotas son:', formulaVeinti)
+		print('Las cuotas que debe realizar en Occidente a 36 cuotas son:', formula36)
+		
+		
+	else:
+		print('El valor no es posible calcular')
+def funcionOccidente4():
+	valorCalcular = int(input('Ingrese el valor que desea ahorrar:'))
+	if valorCalcular>= 200000.01 and valorCalcular <= 500000:
+		interes = 0.0575
+		formulaCompuesto = (((1+(interes/4))**4)-1)
+		bPrimer=(1+formulaCompuesto)**1
+		bSegundo=(1+formulaCompuesto)**2
+		bTercer=(1+formulaCompuesto)**3
+		cPrimer=valorCalcular*bPrimer
+		cSegundo=valorCalcular*bSegundo
+		cTercer=valorCalcular*bTercer
+ 
+		formulaDoce= cPrimer/12
+		formulaVeinti=cSegundo/24
+		formula36=cTercer/36
+	
+		print ('Las cuotas que debe realizar en Occidente a 12 cuotas son:', formulaDoce)
+		print('Las cuotas que debe realizar en Occidente a 24 cuotas son:', formulaVeinti)
+		print('Las cuotas que debe realizar en Occidente a 36 cuotas son:', formula36)
+		
+		
+	else:
+		print('El valor no es posible calcular')
+def funcionOccidente5():
+	valorCalcular = int(input('Ingrese el valor que desea ahorrar:'))
+	if valorCalcular>= 500000.01 and valorCalcular <= 1000000:
+		interes = 0.0575
+		formulaCompuesto = (((1+(interes/4))**4)-1)
+		bPrimer=(1+formulaCompuesto)**1
+		bSegundo=(1+formulaCompuesto)**2
+		bTercer=(1+formulaCompuesto)**3
+		cPrimer=valorCalcular*bPrimer
+		cSegundo=valorCalcular*bSegundo
+		cTercer=valorCalcular*bTercer
+ 
+		formulaDoce= cPrimer/12
+		formulaVeinti=cSegundo/24
+		formula36=cTercer/36
+	
+		print ('Las cuotas que debe realizar en Occidente a 12 cuotas son:', formulaDoce)
+		print('Las cuotas que debe realizar en Occidente a 24 cuotas son:', formulaVeinti)
+		print('Las cuotas que debe realizar en Occidente a 36 cuotas son:', formula36)
+		
+		
+	else:
+		print('El valor no es posible calcular')
+					
+def funcionBAC1():
+	valorCalcular = int(input('Ingrese el valor que desea ahorrar:'))
+	if valorCalcular>= 1000.01 and valorCalcular <= 30000:
+		interes = 0.005
+		formulaCompuesto = (((1+(interes/4))**4)-1)
+		bPrimer=(1+formulaCompuesto)**1
+		bSegundo=(1+formulaCompuesto)**2
+		bTercer=(1+formulaCompuesto)**3
+		cPrimer=valorCalcular*bPrimer
+		cSegundo=valorCalcular*bSegundo
+		cTercer=valorCalcular*bTercer
+ 
+		formulaDoce= cPrimer/12
+		formulaVeinti=cSegundo/24
+		formula36=cTercer/36
+	
+		print ('Las cuotas que debe realizar en BAC a 12 cuotas son:', formulaDoce)
+		print('Las cuotas que debe realizar en BAC a 24 cuotas son:', formulaVeinti)
+		print('Las cuotas que debe realizar en BAC a 36 cuotas son:', formula36)
+		
+		
+	else:
+		print('El valor no es posible calcular')
+					
+def funcionBAC2():
+	valorCalcular = int(input('Ingrese el valor que desea ahorrar:'))
+	if valorCalcular>= 30000.01 and valorCalcular <= 300000:
+		interes = 0.0126
+		formulaCompuesto = (((1+(interes/4))**4)-1)
+		bPrimer=(1+formulaCompuesto)**1
+		bSegundo=(1+formulaCompuesto)**2
+		bTercer=(1+formulaCompuesto)**3
+		cPrimer=valorCalcular*bPrimer
+		cSegundo=valorCalcular*bSegundo
+		cTercer=valorCalcular*bTercer
+ 
+		formulaDoce= cPrimer/12
+		formulaVeinti=cSegundo/24
+		formula36=cTercer/36
+	
+		print ('Las cuotas que debe realizar en BAC a 12 cuotas son:', formulaDoce)
+		print('Las cuotas que debe realizar en BAC a 24 cuotas son:', formulaVeinti)
+		print('Las cuotas que debe realizar en BAC a 36 cuotas son:', formula36)
+		
+		
+	else:
+		print('El valor no es posible calcular')
+					
+def funcionBAC3():
+	valorCalcular = int(input('Ingrese el valor que desea ahorrar:'))
+	if valorCalcular>= 300000.01 and valorCalcular <= 600000:
+		interes = 0.0304
+		formulaCompuesto = (((1+(interes/4))**4)-1)
+		bPrimer=(1+formulaCompuesto)**1
+		bSegundo=(1+formulaCompuesto)**2
+		bTercer=(1+formulaCompuesto)**3
+		cPrimer=valorCalcular*bPrimer
+		cSegundo=valorCalcular*bSegundo
+		cTercer=valorCalcular*bTercer
+ 
+		formulaDoce= cPrimer/12
+		formulaVeinti=cSegundo/24
+		formula36=cTercer/36
+	
+		print ('Las cuotas que debe realizar en BAC a 12 cuotas son:', formulaDoce)
+		print('Las cuotas que debe realizar en BAC a 24 cuotas son:', formulaVeinti)
+		print('Las cuotas que debe realizar en BAC a 36 cuotas son:', formula36)
+		
+		
+	else:
+		print('El valor no es posible calcular')
+					
+def funcionBAC4():
+	valorCalcular = int(input('Ingrese el valor que desea ahorrar:'))
+	if valorCalcular>= 600000.01 and valorCalcular <= 1000000:
+		interes = 0.0356
+		formulaCompuesto = (((1+(interes/4))**4)-1)
+		bPrimer=(1+formulaCompuesto)**1
+		bSegundo=(1+formulaCompuesto)**2
+		bTercer=(1+formulaCompuesto)**3
+		cPrimer=valorCalcular*bPrimer
+		cSegundo=valorCalcular*bSegundo
+		cTercer=valorCalcular*bTercer
+ 
+		formulaDoce= cPrimer/12
+		formulaVeinti=cSegundo/24
+		formula36=cTercer/36
+	
+		print ('Las cuotas que debe realizar en BAC a 12 cuotas son:', formulaDoce)
+		print('Las cuotas que debe realizar en BAC a 24 cuotas son:', formulaVeinti)
+		print('Las cuotas que debe realizaren BAC a 36 cuotas son:', formula36)
+		
+		
+	else:
+		print('El valor no es posible calcular')
+			
+
+def funcionSistema():
+	print('Bienvenido al Sistema:')
+	valorSistema=int(input('Ingrese la cantidad que desea calcular:'))
+	if valorSistema>=5000 and valorSistema <=10000:
+		funcionAtlantida1()
+	elif valorSistema>=500 and valorSistema <=50000:
+		funcionOccidente1()
+	elif valorSistema>=1000 and valorSistema <=30000:
+		funcionBAC1()
+	elif valorSistema>=10000 and valorSistema <=50000:
+		funcionAtlantida2()
+	elif valorSistema>=50000 and valorSistema <=100000:
+		funcionOccidente2()
+	elif valorSistema>=30000 and valorSistema <=300000:
+		funcionBAC2()
+	elif valorSistema>=50000 and valorSistema <=100000:
+		funcionAtlantida3()
+	elif valorSistema>=100000 and valorSistema <=200000:
+		funcionOccidente3()
+	elif valorSistema>=100000 and valorSistema <=500000:
+		funcionAtlantida4()
+	elif valorSistema>=200000 and valorSistema <=50000:
+		funcionOccidente4()
+	elif valorSistema>=300000 and valorSistema <=600000:
+		funcionBAC3()
+	elif valorSistema>=500000 and valorSistema <=1000000:
+		funcionAtlantida5()
+		funcionOccidente5()
+	elif valorSistema>=600000 and valorSistema<=1000000:
+		funcionBAC4()
+	
+	else:
+		print('Valor no disponibles')
+		print('Gracias por preferirnos')
+
+funcionSistema()
